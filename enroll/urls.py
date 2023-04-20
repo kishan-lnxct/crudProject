5054d0f39e4354620ac6d289e277d382bfc38710
kishan-lnxct <kishan@lnxct.com>
2023-04-20 19:05:51 +0530

    add pagination in records

--- enroll/urls.py
+++ enroll/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.add_show, name='add_show'),
-    path('/delete_record/<int:id>', views.delete_record, name='delete_record'),
-    path('<int:id>/', views.update_record, name='update_record'),
+    path('delete_record/<int:id>', views.delete_record, name='delete_record'),
+    path('<int:id>', views.update_record, name='update_record'),
 ]
--- enroll/views.py
+++ enroll/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.urls import reverse
 from .forms import StudentRegistration
 from .models import User
@@ -17,7 +18,21 @@ def add_show(request):
         form = StudentRegistration()
     
     user_data = list(User.objects.all())
-    return render(request, 'enroll/addandshow.html', {'form': form, 'user_data': user_data})
+    p = Paginator(user_data, 4)
+
+    page_number = request.GET.get('page')
+    try:
+        page_obj = p.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = p.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = p.page(p.num_pages)
+
+    context = {'page_obj': page_obj}
+
+    return render(request, 'enroll/addandshow.html', {'form': form, 'user_data': user_data, 'context': context})
 
 def delete_record(request, id):
 
